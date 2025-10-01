3c56b92a69e2ac6eaee89461b52adcf603df220a
Swag12 <164699531+quantumcheatsuser@users.noreply.github.com>
2025-10-01 13:38:49 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,11 @@
---NIGGERS
+-- https://discord.gg/tUEJZYvF9d
+-- known to work on wave, nihon, awp.gg, syn z, seliware
+-- known to work on wave, nihon, awp.gg, syn z, seliware, sirhurt
 local isLimited = ...
 local environment = identifyexecutor and identifyexecutor() or ""
 local source = game:HttpGet("https://raw.githubusercontent.com/quantumcheatsuser/stff/refs/heads/main/" .. (isLimited and "source-limited.lua" or "source.lua"))
 if string.find(string.lower(environment), "wave") and not executed then
-    run_on_actor(getdeletedactors()[1], source)
+    run_on_actor(get_deleted_actors()[1], source)
 elseif getfflag and string.find(string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))), "true") and not executed then
     loadstring(source)()
 elseif string.find(environment, "AWP") ~= nil and not executed then
@@ -29,7 +31,7 @@ elseif string.find(string.lower(environment), "zenith") and not executed then
         ]])
     end
 else
-    queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/quantumcheatsuser/stff/refs/heads/main/hook.lua") .. "task.wait(5);" .. source)
+    queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/iRay888/wapus/refs/heads/main/hook.lua") .. "task.wait(5);" .. source)
     setfflag("DebugRunParallelLuaOnMainThread", "True")
     game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end
